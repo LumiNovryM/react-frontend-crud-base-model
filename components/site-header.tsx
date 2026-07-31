f7e1e882876a5d93f0132ffe1f69b76928a72d24
LumiNovryM <luminovrymekel@gmail.com>
2026-07-31 21:39:53 +0700

    feat: adjust employees, about, contact page and add url detectore

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,7 +1,18 @@
-import { Separator } from "@/components/ui/separator"
-import { SidebarTrigger } from "@/components/ui/sidebar"
+"use client";
+
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { usePathname } from "next/navigation";
+
+const pageTitles: Record<string, string> = {
+  "/dashboard": "Dashboard",
+  "/employees": "Employees",
+  "/about": "About",
+  "/contact": "Contact",
+};
 
 export function SiteHeader() {
+  const pathname = usePathname();
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -10,8 +21,11 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 h-4 data-vertical:self-auto"
         />
-        <h1 className="text-base font-medium">Documents</h1>
+        <h1 className="text-base font-medium">
+          {" "}
+          {pageTitles[pathname] ?? "Unknown Page"}
+        </h1>
       </div>
     </header>
-  )
+  );
 }
