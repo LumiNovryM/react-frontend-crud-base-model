2b0573828e9248005763033bda1b1c0c60d0e7c1
LumiNovryM <luminovrymekel@gmail.com>
2026-07-31 20:42:49 +0700

    feat/chore: setup shadcn & create employees, about, and contact page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
+
+        {/* Base Template Code From ReactJS */}
+        {/* <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
@@ -58,8 +59,10 @@ export default function Home() {
           >
             Documentation
           </a>
-        </div>
+        </div> */}
       </main>
     </div>
   );
 }
+
+export default Home;
