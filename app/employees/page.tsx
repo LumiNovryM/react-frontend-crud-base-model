f1c5bc3b07be744b74802e2bad28535db6d9dbc0
LumiNovryM <luminovrymekel@gmail.com>
2026-08-01 23:36:47 +0700

    Dev: Update UI All Page and Adjust Data Table on Employee Page

--- app/employees/page.tsx
+++ app/employees/page.tsx
@@ -1,9 +1,12 @@
-import { AppSidebar } from "@/components/app-sidebar"
-import { ChartAreaInteractive } from "@/components/chart-area-interactive"
-import { SectionCards } from "@/components/section-cards"
-import { SiteHeader } from "@/components/site-header"
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { DataTable } from "@/components/data-table";
+import { Button } from "@/components/ui/button";
+import { Field } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
 
+import data from "./data.json";
 
 export default function Page() {
   return (
@@ -21,14 +24,18 @@ export default function Page() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
-              <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
+              <div className="grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-linear-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card">
+                <Button variant="outline">Add New Employee</Button>
+                <Field orientation="horizontal">
+                  <Input type="search" placeholder="Search..." />
+                  <Button>Search Employee</Button>
+                </Field>
               </div>
+              <DataTable data={data} />
             </div>
           </div>
         </div>
       </SidebarInset>
     </SidebarProvider>
-  )
+  );
 }
